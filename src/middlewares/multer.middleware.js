99f403de5435ab68769fc1933fa1a491f96cfcab
Deepramakhal <deepra313131@gmail.com>
2024-09-17 12:47:05 +0530

    Multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,15 +1,31 @@
 import multer from "multer";
 
+import fs from 'fs';
+
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-      cb(null, uniqueSuffix + '-' + file.originalname);
+destination: function (req, file, cb) {
+  const dir = './public/temp';
+  fs.mkdir(dir, { recursive: true }, (err) => {
+    if (err) {
+      return cb(err);
     }
-  })
+    cb(null, dir);
+  });
+},
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
+  }
+});
   
-export const upload = multer({ 
-    storage
-})  
+export const upload = multer({
+  storage,
+  limits: { fileSize: 1024 * 1024 * 20 },
+  fileFilter: function (req, file, cb) {
+    if (!file.mimetype.startsWith('image/')) {
+      return cb(new Error('Only image uploads are allowed!'), false);
+    }
+    cb(null, true);
+  }
+});
+   
